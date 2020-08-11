2a95aab739b73bf25b71b996f96b70d99d6b0547
feridun Akyol <feridunakyol34@gmail.com>
2020-08-11 15:58:04 +0200

    deployment

---
+++ build/precache-manifest.aec3cf2dc6df921dda1a08d658d23176.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbf9f54b7f44f7f561285826a346046",
+    "url": "/feridunAKYOL/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "a251b9cf15b4ad068f94",
+    "url": "/feridunAKYOL/Tic-Tac-Toe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4973a055f17149c9dc47",
+    "url": "/feridunAKYOL/Tic-Tac-Toe/static/css/main.a46b6a55.chunk.css"
+  },
+  {
+    "revision": "a251b9cf15b4ad068f94",
+    "url": "/feridunAKYOL/Tic-Tac-Toe/static/js/2.ec1b47b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/feridunAKYOL/Tic-Tac-Toe/static/js/2.ec1b47b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4973a055f17149c9dc47",
+    "url": "/feridunAKYOL/Tic-Tac-Toe/static/js/main.a9227ff5.chunk.js"
+  },
+  {
+    "revision": "eb7e7805dbb48c3b8229",
+    "url": "/feridunAKYOL/Tic-Tac-Toe/static/js/runtime-main.2693f996.js"
+  }
+]);
