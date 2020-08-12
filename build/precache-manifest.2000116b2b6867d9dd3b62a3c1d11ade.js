3e2415e6ca679004ea97dee3ff261f5983da02aa
feridun Akyol <feridunakyol34@gmail.com>
2020-08-12 19:06:06 +0200

    last version

---
+++ build/precache-manifest.2000116b2b6867d9dd3b62a3c1d11ade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b6637194a00d47daebb4b5c9b68aa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9cede84e44d5fdf9403",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c2a62cc29ba91a15a76f",
+    "url": "/static/css/main.a46b6a55.chunk.css"
+  },
+  {
+    "revision": "f9cede84e44d5fdf9403",
+    "url": "/static/js/2.e32b72b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e32b72b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a62cc29ba91a15a76f",
+    "url": "/static/js/main.4a0597ab.chunk.js"
+  },
+  {
+    "revision": "8d732de9640caad833bf",
+    "url": "/static/js/runtime-main.b2b4369a.js"
+  }
+]);
